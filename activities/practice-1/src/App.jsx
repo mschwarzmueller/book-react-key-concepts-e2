6214dc6b802962f05c5881c19ff700597e137c93
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2024-05-27 15:34:41 +0200

    migrated to Vite

---
+++ activities/practice-1/src/App.jsx
@@ -0,0 +1,17 @@
+import Add from './components/Add.jsx';
+import Divide from './components/Divide.jsx';
+import Multiply from './components/Multiply.jsx';
+import Subtract from './components/Subtract.jsx';
+
+function App() {
+  return (
+    <>
+      <Add />
+      <Subtract />
+      <Multiply />
+      <Divide />
+    </>
+  );
+}
+
+export default App;
---
+++ activities/practice-1/src/components/Multiply.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Multiply() {
+  const [enteredNumbers, setEnteredNumbers] = useState({ first: 0, second: 0 });
+
+  function changeFirstNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: +event.target.value,
+      second: prevNumbers.second,
+    }));
+  }
+
+  function changeSecondNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: prevNumbers.first,
+      second: +event.target.value,
+    }));
+  }
+
+  const result = enteredNumbers.first * enteredNumbers.second;
+
+  return (
+    <p>
+      <input type="number" onChange={changeFirstNumberHandler} /> *{' '}
+      <input type="number" onChange={changeSecondNumberHandler} /> = {result}
+    </p>
+  );
+}
+
+export default Multiply;
